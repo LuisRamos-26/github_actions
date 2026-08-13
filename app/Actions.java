410e362c4cedd51d21cd4f67aafa31f4ee5ef293
LuisRamos-26 <soporte.ulaes@gmail.com>
2026-08-12 20:30:47 -0600

    hoal

--- app/Actions.java
+++ app/Actions.java
@@ -1,6 +1,6 @@
 public class Actions {
     public static void main(String[] args){
         System.out.println("hola mundo desde java con github Actions");
-        System.out.println("Ejecutando workflow java.yml");
+        System.out.println("Ejecutando workflow de java.yml");
     }
 }
